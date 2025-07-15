55a572fbc4e1468a1a56cdbfe8e2c1caf34e3877
BalvesD3v <lpbalves7filho@gmail.com>
2025-07-14 22:13:48 -0300

    Add Spinner component and integrate with Loader and FormGroup

--- src/Components/FormGroup/index.js
+++ src/Components/FormGroup/index.js
@@ -1,12 +1,17 @@
 import PropTypes from "prop-types";
 import { Contanier } from "./styles";
+import { Spinner } from "../Spinner";
 
 export default function FormGroup({ children, error, isLoading }) {
   return (
     <Contanier>
       <div className="form-item">
         {children}
-        {isLoading && <div className="loader"></div>}
+        {isLoading && (
+          <div className="loader">
+            <Spinner size={16} />
+          </div>
+        )}
       </div>
       {error && <small> {error} </small>}
     </Contanier>
