6e0599a85f7e14141f90bb25597349936deb4408
BalvesD3v <lpbalves7filho@gmail.com>
2025-07-28 21:57:15 -0300

    Refactor toast notification system to use EventManager for event handling; remove direct DOM event listeners

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,54 +1,37 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {};
+    this.listeners = new Map();
   }
 
   on(event, listener) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
     }
 
-    this.listeners[event].push(listener);
+    this.listeners.get(event).push(listener);
   }
 
   emit(event, payload) {
-    if (!this.listeners[event]) {
+    if (!this.listeners.has(event)) {
       return;
     }
 
-    this.listeners[event].forEach((listener) => {
+    this.listeners.get(event).forEach((listener) => {
       listener(payload);
     });
   }
 
   removeListener(event, listenerToRemove) {
-    const listeners = this.listeners[event];
+    const listeners = this.listeners.get(event);
 
-    if (!this.listeners[event]) {
+    if (!listeners) {
       return;
     }
 
-    const filteredListeners = this.listeners[event].filter(
+    const filteredListeners = listeners.filter(
       (listener) => listener !== listenerToRemove
     );
 
-    this.listeners[event] = filteredListeners;
+    this.listeners.set(event, filteredListeners);
   }
 }
-
-const toastEventManager = new EventManager();
-
-function addtoast1(payload) {
-  console.log("addtoast listener1", payload);
-}
-function addtoast2(payload) {
-  console.log("addtoast listener2", payload);
-}
-
-toastEventManager.on("addtoast", addtoast1);
-toastEventManager.on("addtoast", addtoast2);
-toastEventManager.emit("addtoast", { type: "danger", text: "texto" });
-
-toastEventManager.removeListener("addtoast", addtoast1);
-toastEventManager.emit("addtoast", "tasdfasdexto...");
-console.log(toastEventManager);
